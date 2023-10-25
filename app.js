df01e9fa4719a5df6b713f040f63b78d6a07f1ad
MichalZalewski92 <michal.soski@gmail.com>
2023-10-25 17:06:23 +0200

    aircraft(user) and its movement implemented

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ player.style.left = playerX + "px";
 player.style.top = playerY + "px";
 
 
-window.addEventListener("keydown", (event) => {
+document.addEventListener("keydown", (event) => {
   if (event.key === "ArrowLeft") {
     playerX -= 10;
   } else if (event.key === "ArrowRight") {
