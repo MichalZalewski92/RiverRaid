2675720f7b8b9894460cdb561b5ef2591342ea69
MichalZalewski92 <michal.soski@gmail.com>
2023-10-27 10:25:49 +0200

    changes

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
 const player = document.getElementById("player");
+const gameContainer = document.getElementById("game-container");
 
 
 let playerX = 375;
 let playerY = 500;
+let isShooting = false;
 player.style.left = playerX + "px";
 player.style.top = playerY + "px";
 
+
+
+function shoot() {
+    if (!isShooting) {
+      isShooting = true;
+      const bullet = document.createElement("div");
+      bullet.className = "bullet";
+      bullet.style.left = playerX + playerWidth / 2 - 2.5 + "px";
+      bullet.style.bottom = "50px";
+      gameContainer.appendChild(bullet);
+
+      const bulletMoveInterval = setInterval(() => {
+        const bulletBottom = parseInt(bullet.style.bottom) || 0;
+        bullet.style.bottom = bulletBottom + 10 + "px";
+
+        if (bulletBottom >= gameContainer.clientHeight) {
+          gameContainer.removeChild(bullet);
+          isShooting = false;
+          clearInterval(bulletMoveInterval);
+        }
+      }, 1000 / 60);
+    }
+  }
 
 document.addEventListener("keydown", (event) => {
   if (event.key === "ArrowLeft") {
